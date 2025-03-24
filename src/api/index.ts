f47abd71601e0df3aee8f4812cb79072a4bf267c
wu <zhmywzm@zohomail.cn>
2025-03-24 10:40:15 +0800

    first commit new project 第一次提交新项目

---
+++ src/api/index.ts
@@ -0,0 +1,11 @@
+/*
+ * @Author: nian
+ * @Date: 2024-12-23 11:18:37
+
+ * @LastEditTime: 2025-01-06 16:07:20
+ * @FilePath: \web-pc-svn\src\api\modules\index.js
+ */
+
+import {GETNOBASE} from "./api";
+export * from "./modules/index"
+export {GETNOBASE}
